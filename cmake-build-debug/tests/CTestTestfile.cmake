ec560f624cdaf719fb725e1311fc41fce33beb1d
Nike486 <126013542+Nike486@users.noreply.github.com>
2023-03-08 20:16:15 +0300

    Delete cmake-build-debug directory

--- cmake-build-debug/tests/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/evgen/Desktop/search_engine/tests
-# Build directory: C:/Users/evgen/Desktop/search_engine/cmake-build-debug/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("C:/Users/evgen/Desktop/search_engine/cmake-build-debug/tests/GtestTests[1]_include.cmake")
